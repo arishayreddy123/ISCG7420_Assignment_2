7f3c8793cf0eea20fe72db2a34cb677fe4e040e7
Arishay <arishay.r16@gmail.com>
2025-05-14 12:04:18 +1200

    add viewsets

--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Room, Reservation
+from .serializers import RoomSerializer, ReservationSerializer
 
-# Create your views here.
+class RoomViewSet(viewsets.ModelViewSet):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
